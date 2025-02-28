64ee86dff27ec7382bd9ac35f0410f2d1c00c60e
Huffman821 <oneninesoccer@gmail.com>
2025-02-28 12:50:21 -0500

    Add files via upload

---
+++ project/required/Project2requirements.R
@@ -0,0 +1,6 @@
+install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("tidyverse",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("caret",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("Metrics",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+
+
